dab3742d3a8b3c797639237d3f89aa62c45c1330
SHWETA_1564 <shwetaydv08@gmail.com>
2025-08-12 00:59:03 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable{
+    public static void main(String[] args){
+        int helloAnj=81;
+        int _gl=65;
+        int $12=11;
+        int sum=helloAnj+_gl;
+        System.out.println(sum);
+    }
+}
