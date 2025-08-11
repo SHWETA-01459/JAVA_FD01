f65cb64b4db7e982f26aa033006036ce3547cc49
SHWETA_1564 <shwetaydv08@gmail.com>
2025-08-12 00:34:41 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype{
+    public static void main(String[] args){
+        int a=10;
+        float b=6.1f;
+        double c=2.8776;
+        char d='q';
+        boolean e=true;
+        byte f=100;
+        short z=123;
+        long y=123456789;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(z);
+        System.out.println(y);
+    }
+}
